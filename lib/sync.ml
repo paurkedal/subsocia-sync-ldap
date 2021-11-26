2f94fa79194949ec5c2415c2b9c05118dbb6c135
Petter A. Urkedal <urkedal@nbi.dk>
2021-11-26 17:48:59 +0100

    Refactor Sync.process_scope.

--- lib/sync.ml
+++ lib/sync.ml
@@ -83,79 +83,25 @@ let format_ptime fmt (t, tz_offset_s) tz_offset_s_cfg =
   in
   Netdate.create ~zone (Ptime.to_float_s t) |> Netdate.format ~fmt
 
-let rec process_scope
-    ~config ~period ?partition_lb ?partition_ub ~ldap_conn ~target_cache
-    scope_name =
-  let retry_period period =
-    process_scope
-      ~config ~period ?partition_lb ?partition_ub
-      ~ldap_conn ~target_cache scope_name
-  in
-  let retry_partition partition_lb partition_ub =
-    process_scope
-      ~config ~period ?partition_lb ?partition_ub
-      ~ldap_conn ~target_cache scope_name
-  in
+let make_time_filters ~scope_name update_time_filter period =
+  (match period, update_time_filter with
+   | (None, None), None -> []
+   | (_, _), None ->
+      Fmt.failwith "No update time filter provided for scope %s" scope_name
+   | (tI, tF), Some (fitI, fitF, fmt, tz) ->
+      let mk_time_filter fit t =
+        let t_str = format_ptime fmt t tz in
+        Netldapx.Filter_template.expand
+          (function
+           | "t" -> t_str
+           | x -> Fmt.failwith "Undefined variable %s." x)
+          fit
+      in
+      [] |> Option.fold (List.cons % mk_time_filter fitI) tI
+         |> Option.fold (List.cons % mk_time_filter fitF) tF)
+
+let process_scope ~config ~period ~ldap_conn ~target_cache scope_name =
   let scope = Dict.find scope_name config.Config.scopes in
-
-  (* Combine global and scope filters *)
-  let filter =
-    (match config.Config.ldap_filters, scope.Config.ldap_filters with
-     | [], [] -> failwith "No LDAP filter provided."
-     | [], sfilters -> `And sfilters
-     | cfilters, [] -> `And cfilters
-     | cfilters, tfilters -> `And (tfilters @ cfilters))
-  in
-
-  (* Add update time filters *)
-  let ldap_update_time_filter =
-    (match scope.Config.ldap_update_time_filter with
-     | Some _ as fit -> fit
-     | None -> config.Config.ldap_update_time_filter)
-  in
-  let filter =
-    (match period, ldap_update_time_filter with
-     | (None, None), None -> filter
-     | (_, _), None ->
-        Fmt.failwith "No update time filter provided for scope %s" scope_name
-     | (tI, tF), Some (fitI, fitF, fmt, tz) ->
-        let mk_time_filter fit t =
-          let t_str = format_ptime fmt t tz in
-          Netldapx.Filter_template.expand
-            (function
-             | "t" -> t_str
-             | x -> Fmt.failwith "Undefined variable %s." x)
-            fit
-        in
-        let subfilters =
-          [filter]
-            |> Option.fold (List.cons % mk_time_filter fitI) tI
-            |> Option.fold (List.cons % mk_time_filter fitF) tF
-        in
-        `And subfilters)
-  in
-  let filter =
-    (match scope.Config.ldap_partition_attribute_type with
-     | None -> filter
-     | Some attr_type ->
-        let ge v = `Greater_or_equal (attr_type, v) in
-        (match partition_lb, partition_ub with
-         | None, None -> filter
-         | Some lb, None -> `And [filter; ge lb]
-         | None, Some ub -> `And [filter; `Not (ge ub)]
-         | Some lb, Some ub -> `And [filter; ge lb; `Not (ge ub)]))
-  in
-
-  Log.info (fun m -> m "Scope %s %a:" scope_name pp_period period)
-    >>= fun () ->
-  Log.debug (fun m -> m "LDAP base: %s" scope.Config.ldap_base_dn)
-    >>= fun () ->
-  Log.debug (fun m -> m "LDAP scope: %s"
-                            (Netldapx.string_of_scope scope.Config.ldap_scope))
-    >>= fun () ->
-  Log.debug (fun m -> m "LDAP filter: %s" (Netldapx.string_of_filter filter))
-    >>= fun () ->
-
   let targets =
     let get_conn target_name =
       try Hashtbl.find target_cache target_name
@@ -166,86 +112,128 @@ let rec process_scope
     in
     List.map get_conn scope.Config.target_names
   in
+  let attributes =
+    String_set.empty
+      |> List.fold (List.fold String_set.add % Target.ldap_attributes) targets
+  in
 
-  let* lr =
-    let attributes =
-      String_set.empty
-        |> List.fold (List.fold String_set.add % Target.ldap_attributes) targets
+  (* Combine global and scope filters *)
+  let fixed_filter =
+    (match config.Config.ldap_filters, scope.Config.ldap_filters with
+     | [], [] -> failwith "No LDAP filter provided."
+     | [], sfilters -> `And sfilters
+     | cfilters, [] -> `And cfilters
+     | cfilters, tfilters -> `And (tfilters @ cfilters))
+  in
+  let update_time_filter =
+    (match scope.Config.ldap_update_time_filter with
+     | Some _ as fit -> fit
+     | None -> config.Config.ldap_update_time_filter)
+  in
+
+  let rec recurse period partition_lb partition_ub =
+    let filter =
+      (match make_time_filters ~scope_name update_time_filter period with
+       | [] -> fixed_filter
+       | filters -> `And (fixed_filter :: filters))
     in
-    Lwt_preemptive.detach
-      (Netldap.search ldap_conn
-        ~base:scope.Config.ldap_base_dn
-        ~scope:scope.Config.ldap_scope
-        ~deref_aliases:`Always
-        ~size_limit:(Option.get_or 0 scope.Config.ldap_size_limit)
-        ~time_limit:(Option.get_or 0 scope.Config.ldap_time_limit)
-        ~types_only:false
-        ~filter
-        ~attributes:(String_set.elements attributes))
-      ()
+    let filter =
+      (match scope.Config.ldap_partition_attribute_type with
+       | None -> filter
+       | Some attr_type ->
+          let ge v = `Greater_or_equal (attr_type, v) in
+          (match partition_lb, partition_ub with
+           | None, None -> filter
+           | Some lb, None -> `And [filter; ge lb]
+           | None, Some ub -> `And [filter; `Not (ge ub)]
+           | Some lb, Some ub -> `And [filter; ge lb; `Not (ge ub)]))
+    in
+    Log.info (fun f ->
+      f "Scope %s %a:" scope_name pp_period period) >>= fun () ->
+    Log.debug (fun f ->
+      f "LDAP base: %s" scope.Config.ldap_base_dn) >>= fun () ->
+    Log.debug (fun f ->
+      f "LDAP scope: %s" (Netldapx.string_of_scope scope.Config.ldap_scope))
+      >>= fun () ->
+    Log.debug (fun f ->
+      f "LDAP filter: %s" (Netldapx.string_of_filter filter)) >>= fun () ->
+    let* lr =
+      Lwt_preemptive.detach
+        (Netldap.search ldap_conn
+          ~base:scope.Config.ldap_base_dn
+          ~scope:scope.Config.ldap_scope
+          ~deref_aliases:`Always
+          ~size_limit:(Option.get_or 0 scope.Config.ldap_size_limit)
+          ~time_limit:(Option.get_or 0 scope.Config.ldap_time_limit)
+          ~types_only:false
+          ~filter
+          ~attributes:(String_set.elements attributes))
+        ()
+    in
+    let process_targets entries =
+      Lwt_list.iter_p (fun target -> Target.process target entries) targets
+    in
+    (match lr#code with
+     | `Success ->
+        process_targets lr#value >>= fun () ->
+        Lwt.return_none
+     | `TimeLimitExceeded ->
+        Log.err (fun m ->
+          m "Result for %s is incomplete due to time limit."
+            scope_name) >>= fun () ->
+        let+ () =
+          if not scope.Config.partial_is_ok then Lwt.return_unit else
+          process_targets lr#partial_value
+        in
+        Some (scope_name, `Time_limit_exceeded)
+     | `SizeLimitExceeded ->
+        (match period, scope.Config.ldap_partition_attribute_type with
+         | ((Some (tI, tzI) as tI'), (Some (tF, _) as tF')), _ when
+              Ptime.Span.compare (Ptime.diff tF tI)
+                                 config.Config.min_update_period > 0 ->
+            Log.warn (fun f ->
+              f "Size limit exceeded for period %a, splitting period."
+                pp_period period) >>= fun () ->
+            let dtIM = Option.get @@ Ptime.Span.of_float_s @@
+              0.5 *. Ptime.Span.to_float_s (Ptime.diff tF tI) in
+            let tM' = Some (Ptime.add_span tI dtIM |> Option.get, tzI) in
+            (match%lwt recurse (tI', tM') None None with
+             | None -> recurse (tM', tF') None None
+             | Some err -> Lwt.return_some err)
+         | _, Some attr_type ->
+            let entries = lr#partial_value in
+            (match List.nth entries (Random.int (List.length entries)) with
+             | `Entry (_, attrs) ->
+                (match List.assoc_opt attr_type attrs with
+                 | Some (partition :: _) ->
+                    Log.warn (fun f ->
+                      f "Size limit exceeded for period %a, splitting on %s=%s."
+                        pp_period period attr_type partition) >>= fun () ->
+                    (match%lwt recurse period partition_lb (Some partition) with
+                     | None -> recurse period (Some partition) partition_ub
+                     | Some err -> Lwt.return_some err)
+                 | Some [] | None ->
+                    Log.err (fun f ->
+                      f "Size limit exceeded and no attribute value to split on.")
+                      >|= fun () ->
+                    Some (scope_name, `Size_limit_exceeded))
+             | `Reference _ -> assert false)
+         | _ ->
+            Log.err (fun m ->
+              m "Result for %s is incomplete due to size limit." scope_name)
+              >>= fun () ->
+            let+ () =
+              if not scope.Config.partial_is_ok then Lwt.return_unit else
+              process_targets lr#partial_value
+            in
+            Some (scope_name, `Size_limit_exceeded))
+     | _ ->
+        Log.err (fun m ->
+          m "LDAP search for scope %s failed: %s" scope_name lr#diag_msg)
+          >>= fun () ->
+        Lwt.return_some (scope_name, `Search_failed))
   in
-  let process_targets entries =
-    Lwt_list.iter_p (fun target -> Target.process target entries) targets
-  in
-  (match lr#code with
-   | `Success ->
-      process_targets lr#value >>= fun () ->
-      Lwt.return_none
-   | `TimeLimitExceeded ->
-      Log.err (fun m ->
-        m "Result for %s is incomplete due to time limit."
-          scope_name) >>= fun () ->
-      let+ () =
-        if not scope.Config.partial_is_ok then Lwt.return_unit else
-        process_targets lr#partial_value
-      in
-      Some (scope_name, `Time_limit_exceeded)
-   | `SizeLimitExceeded ->
-      (match period, scope.Config.ldap_partition_attribute_type with
-       | ((Some (tI, tzI) as tI'), (Some (tF, _) as tF')), _ when
-            Ptime.Span.compare (Ptime.diff tF tI)
-                               config.Config.min_update_period > 0 ->
-          Log.warn (fun f ->
-            f "Size limit exceeded for period %a, splitting period."
-              pp_period period) >>= fun () ->
-          let dtIM = Option.get @@ Ptime.Span.of_float_s @@
-            0.5 *. Ptime.Span.to_float_s (Ptime.diff tF tI) in
-          let tM' = Some (Ptime.add_span tI dtIM |> Option.get, tzI) in
-          (match%lwt retry_period (tI', tM') with
-           | None -> retry_period (tM', tF')
-           | Some err -> Lwt.return_some err)
-       | _, Some attr_type ->
-          let entries = lr#partial_value in
-          (match List.nth entries (Random.int (List.length entries)) with
-           | `Entry (_, attrs) ->
-              (match List.assoc_opt attr_type attrs with
-               | Some (partition :: _) ->
-                  Log.warn (fun f ->
-                    f "Size limit exceeded for period %a, splitting on %s=%s."
-                      pp_period period attr_type partition) >>= fun () ->
-                  (match%lwt retry_partition partition_lb (Some partition) with
-                   | None -> retry_partition (Some partition) partition_ub
-                   | Some err -> Lwt.return_some err)
-               | Some [] | None ->
-                  Log.err (fun f ->
-                    f "Size limit exceeded and no attribute value to split on.")
-                    >|= fun () ->
-                  Some (scope_name, `Size_limit_exceeded))
-           | `Reference _ -> assert false)
-       | _ ->
-          Log.err (fun m ->
-            m "Result for %s is incomplete due to size limit." scope_name)
-            >>= fun () ->
-          let+ () =
-            if not scope.Config.partial_is_ok then Lwt.return_unit else
-            process_targets lr#partial_value
-          in
-          Some (scope_name, `Size_limit_exceeded))
-   | _ ->
-      Log.err (fun m ->
-        m "LDAP search for scope %s failed: %s" scope_name lr#diag_msg)
-        >>= fun () ->
-      Lwt.return_some (scope_name, `Search_failed))
+  recurse period None None
 
 type scope_error =
  [ `Search_failed
